1ba4033de5f76cb358976d0bc4b0432973776e27
and-rs <deserd@protonmail.com>
2025-03-13 23:20:57 -0500

    testing structs and strings

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
 
+const Test = struct {
+    hello: []const u8,
+
+    fn structFunction() []const u8 {
+        return "hello from fn";
+    }
+};
+
 pub fn main() !void {
+    const testing_struct = Test{ .hello = "this is the new struct" };
+
     std.debug.print("hello from zig \n", .{});
+    std.debug.print("testing struct => {s} \n", .{testing_struct.hello});
 }
